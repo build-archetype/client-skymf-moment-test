bb2ad74f63272e4f6bb5cf5c4aa0c5cec5dc0138
swb-buildarchetype <sam@wardbiddle.xyz>
2024-10-22 10:28:04 -0400

    Remove NEXT URL REFERNCE.

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,6 +2,7 @@ import { getPayload } from "payload/dist/payload";
 import nodemailer from "nodemailer";
 import config from "./payload.config";
 import { MongoClient } from "mongodb";
+import { InitOptions } from "payload/config";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.resend.com",
@@ -19,7 +20,9 @@ if (!cached) {
   cached = (global as any).payload = { client: null, promise: null };
 }
 
-export const getPayloadClient = async () => {
+export const getPayloadClient = async (options?: {
+  initOptions?: Partial<InitOptions>;
+}) => {
   if (cached.client) {
     return cached.client;
   }
@@ -36,6 +39,7 @@ export const getPayloadClient = async () => {
         fromName: "moment",
         fromAddress: "hello@moment.com",
       },
+      ...(options?.initOptions || {}),
     });
   }
 
